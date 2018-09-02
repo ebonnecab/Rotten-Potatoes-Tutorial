e1b591f178358e6ec04ab5970ba691b7d395ceae
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-09-02 16:51:00 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  
-  res.render('home', { msg: 'Hello World!' });
-})
+
+//app.get('/', (req, res) => {
+
+ // res.render('home', { msg: 'Hello World!' });
+//})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
@@ -14,3 +15,14 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
