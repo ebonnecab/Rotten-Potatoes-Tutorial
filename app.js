6ecfce517555c8c7472425855b62543643906f56
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-09-10 17:41:32 -0700

    bugs fixed

--- app.js
+++ app.js
@@ -2,67 +2,75 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/reviews', (req, res) => {
-    Review.create(req.body).then((review) => {
-      console.log(review)
-      res.redirect(`/reviews/${review._id}`)
-    }).catch((err) => {
-      console.log(err.message)
-    })
-  })
-  // SHOW
-  app.get('/reviews/:id', (req, res) => {
-   Review.findById(req.params.id).then((review) => {
-     res.render('reviews-show', { review: review })
-   }).catch((err) => {
-     console.log(err.message);
-   })
- })
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', {
+    userNewUrlParser: true
+});
 
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-let reviews = [
-  { title: "Great Review" },
-  { title: "Bad Review" }
-]
+const Review = mongoose.model('Review', {
+    title: String,
+    description: String,
+    movieTitle: String,
+    movieRatings: Number
+});
+
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+    console.log(req.params);
+
+    res.render('reviews-new', {});
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+    console.log('my id:'+ req.params.id);
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', {
+            review: review
+        })
+    }).catch((err) => {
+        console.log(err.message);
+    });
+});
+
 
 // INDEX
 app.get('/', (req, res) => {
     Review.find()
-      .then(reviews => {
-  res.render('reviews-index', { reviews: reviews });
-})
-.catch(err => {
-      console.log(err);
-    })
-})
-
-// var reviews = [
-//   { title: "Great Review" },
-//   { title: "Next Review" }
-// ]
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
-
-const Review = mongoose.model('Review', {
-  title: String,
-  description: String,
-  movieTitle: String,
-  movieRatings: String
+        .then(reviews => {
+            res.render('reviews-index', {
+                reviews: reviews
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-// NEW
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
+
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!')
 })
