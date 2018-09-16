a8118378f4031efc04c2a953b6f1071a5cff8590
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-09-16 13:26:29 -0700

    Added vanilla bootstrap

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -60,23 +60,21 @@ describe('Reviews', () => {
                 });
         });
 
-    });
+        describe('Reviews', () => {
 
-    describe('Reviews', () => {
+            after(() => {
+                Review.deleteMany({
+                    title: 'Super Sweet Review'
+                }).exec((err, reviews) => {
+                    console.log(reviews)
+                    reviews.remove();
+                })
+            });
 
-        after(() => {
-            Review.deleteMany({
-                title: 'Super Sweet Review'
-            }).exec((err, reviews) => {
-                console.log(reviews)
-                reviews.remove();
-            })
+
         });
 
-
     });
-
-
     // TEST EDIT
     it('should edit a SINGLE review on /reviews/<id>/edit GET', (done) => {
         var review = new Review(sampleReview);
@@ -92,29 +90,31 @@ describe('Reviews', () => {
     });
     // TEST UPDATE
     it('should update a SINGLE review on /reviews/<id> PUT', (done) => {
-    var review = new Review(sampleReview);
-    review.save((err, data)  => {
-     chai.request(app)
-      .put(`/reviews/${data._id}?_method=PUT`)
-      .send({'title': 'Updating the title'})
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.html
-        done();
-      });
+        var review = new Review(sampleReview);
+        review.save((err, data) => {
+            chai.request(app)
+                .put(`/reviews/${data._id}?_method=PUT`)
+                .send({
+                    'title': 'Updating the title'
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
     });
-  });
     // TEST DELETE
     it('should delete a SINGLE review on /reviews/<id> DELETE', (done) => {
-    var review = new Review(sampleReview);
-    review.save((err, data)  => {
-     chai.request(app)
-      .delete(`/reviews/${data._id}?_method=DELETE`)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.html
-        done();
-      });
+        var review = new Review(sampleReview);
+        review.save((err, data) => {
+            chai.request(app)
+                .delete(`/reviews/${data._id}?_method=DELETE`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
     });
-  });
 });
